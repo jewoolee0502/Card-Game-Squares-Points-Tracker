8fde2cf313245909dfe99bae53c0837fd1884798
jewoolee0502 <jwcairo0502@gmail.com>
2021-05-05 06:41:07 +0200

    Updated

--- src/PointsCounter.java
+++ src/PointsCounter.java
@@ -1,2 +1,36 @@
+import java.util.*;
+import java.lang.*;
+
 public class PointsCounter {
+    private static String playerName1;
+    private static String playerName2;
+    private static String playerName3;
+    private static int points1 = 0;
+    private static int points2 = 0;
+    private static int points3 = 0;
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
+
+    public static void gameSetUp(Scanner input) {
+        System.out.println("Enter the player names: ");
+        System.out.print("> Player 1: ");
+        playerName1 = input.nextLine();
+        System.out.print("> Player 2: ");
+        playerName2 = input.nextLine();
+        System.out.print("> Player 3: ");
+        playerName3 = input.nextLine();
+
+        if(playerName1 == null || playerName2 == null || playerName3 == null) {
+            System.out.println("One of the player's name has not been entered.");
+            System.exit(0);
+        }
+        System.out.println("---------------------------------------------------------------------------------");
+    }
+
+    public static void game(Scanner input) {
+
+    }
+
 }
